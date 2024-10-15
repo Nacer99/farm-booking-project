b575c9ee90a23aca5b6aa48733d71e4b4ce88784
Nacer99 <66947467+Nacer99@users.noreply.github.com>
2024-10-15 11:45:47 +0000

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,9 @@ app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/farms', farmRoutes);
