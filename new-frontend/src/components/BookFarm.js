b544ecb1fdbdc8fecab3fa7d02f6df91b259ecd2
Nacer99 <66947467+Nacer99@users.noreply.github.com>
2024-10-18 15:07:45 +0000

    use state duplica deleted

--- new-frontend/src/components/BookFarm.js
+++ new-frontend/src/components/BookFarm.js
@@ -6,8 +6,6 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../styles/BookFarm.css';
 
-import { useState, useEffect } from 'react';
-
 function BookFarm() {
   const [farms, setFarms] = useState([]);
 
